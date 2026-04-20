95fba775d0aede20a335fdafc371d959683eb2f2
Truonghocdot <truonghocdot05@gmail.com>
2026-04-20 16:48:10 +0700

    Implement

--- lang/vi/admin.php
+++ lang/vi/admin.php
@@ -812,7 +812,7 @@ return
                 'max_uses' => 'Số lần sử dụng',
                 'is_percentage' => 'Loại giảm giá',
                 'description' => 'Mô tả',
-                'count_collect' => 'Giới hạn thu thập',
+                'count_collect' => 'Lượng thu thập',
                 'discount_value' => 'Giá trị giảm ( điểm/ %)',
                 'max_discount' => 'Số điểm tối đa',
                 'usage_limit' => 'Số lần áp dụng tối đa',
